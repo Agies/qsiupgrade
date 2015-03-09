8571332310d30844f80a55869178d14b91c64a18
Jerel R Hass <jhass@quicksolutions.com>
2015-03-09 11:24:47 -0400

    Initial

---
+++ qsiupgrade/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qsiupgrade.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndexModel model)
+        {
+            return View();
+        }
+
+    }
+
+    public class IndexModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Company { get; set; }
+    }
+}
